f7493ce8446ed39ccd609e551e22881dd32e40ff
Doxygen CI <ci-arduino@invalid>
2021-09-22 16:01:46 +0000

    Deploy docs to GitHub Pages from commit 67ea87afb6
    Commit: 67ea87afb655c7a4697468a087470458b30d75e8
    GitHub Actions run: 1262518445

--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -3,5 +3,6 @@ var searchData=
   ['setpin',['setPin',['../class_adafruit___motor_shield.html#a3cde03826b704170be651261bd4e5196',1,'Adafruit_MotorShield']]],
   ['setpwm',['setPWM',['../class_adafruit___motor_shield.html#ac228285ca308002deb4fb3f839d5cb44',1,'Adafruit_MotorShield']]],
   ['setspeed',['setSpeed',['../class_adafruit___d_c_motor.html#a237f54e43a94ee9836fbb8559ba3b7cc',1,'Adafruit_DCMotor::setSpeed()'],['../class_adafruit___stepper_motor.html#a55a75e9f3589c4745fb81bb1432b8895',1,'Adafruit_StepperMotor::setSpeed()']]],
+  ['setspeedfine',['setSpeedFine',['../class_adafruit___d_c_motor.html#ac24f321579fbee4fa6ccf4443597ed26',1,'Adafruit_DCMotor']]],
   ['step',['step',['../class_adafruit___stepper_motor.html#a2d4727065d8a0c8346fa85771327baf4',1,'Adafruit_StepperMotor']]]
 ];
